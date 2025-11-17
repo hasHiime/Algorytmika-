aa9307f37ceaf4c42fc981dcb1e680d732a9b2ab
Hashi <148062357+hasHiime@users.noreply.github.com>
2025-11-17 09:00:09 +0100

    Added zadanie 7

---
+++ Zadanie7algorytmMetodaPolowieniaPrzedzialow.py
@@ -0,0 +1,34 @@
+def f(x):
+    return x*(x*(x-3)+2)-6
+
+a = -10
+b = 10
+epsilon = 0.00001
+
+# def PolowieniePrzedzialow(a, b, epsilon):
+#     srodek = (a+b)/2
+#     if f(a) == 0.0:
+#         return a
+#     elif f(b) == 0.0:
+#         return b
+#     elif b-a <= epsilon:
+#         return srodek
+#     elif f(a)*f(srodek) < 0:
+#         return PolowieniePrzedzialow(a, srodek, epsilon)
+#     return PolowieniePrzedzialow(srodek, b, epsilon)
+
+def PolowieniePrzedzialow(a, b, epsilon):
+    if f(a) == 0.0:
+        return a
+    elif f(b) == 0.0:
+        return b
+    while b-a > epsilon:
+        srodek = (a+b)/2
+        if f(srodek) == 0:
+            return srodek
+        elif f(a)*f(srodek) < 0:
+            b = srodek
+        else:
+            a = srodek
+    return (a+b)/2        
+print("Znalezione miejsce zerowe wynosi: ", PolowieniePrzedzialow(a, b, epsilon))
